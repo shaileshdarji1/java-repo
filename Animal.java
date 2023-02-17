3f6b89d50a544f79bfd8b8462222ada670643eab
shailesh <shailesh.d@simformsolutions.com>
2023-02-17 14:19:08 +0530

    This is mihir branch

--- Animal.java
+++ Animal.java
@@ -2,6 +2,7 @@ public Class Animal{
 public static void main(String args[])
 {
    System.out.println("This is animal class");
-   System.out.println('This second branch commit');
+   System.out.println("This is first branch");
+   System.out.println("This is mihir branch");
 }
 }
